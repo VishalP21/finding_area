9856a212f27b10cf480de44f0dbc84839009eb10
Vishal Prajapati <vishalp951295@gmail.com>
2024-07-13 09:49:50 +0530

    Add project files.

---
+++ finding_area/Program.cs
@@ -0,0 +1,8 @@
+﻿using finding_area;
+
+area area = new();
+area.sum(10);
+area.sum(8.4);
+area.sum(5, 8);
+area.sum(14.68);
+area.sum(2);
---
+++ finding_area/area.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finding_area
+{
+    public class area
+    {
+        public int sq_area = 0;
+        public int rct_area = 0;
+        public double cir_area = 0;
+        public void sum(int l)
+        {
+            sq_area = l * l;
+            Console.WriteLine(sq_area);
+        }
+        public void sum(int x, int y)
+        {
+            rct_area = x * y;
+            Console.WriteLine(rct_area);
+        }
+        public void sum(double a)
+        {
+            cir_area = (22/7) * a;
+            Console.WriteLine(cir_area);
+        }
+    }
+}
